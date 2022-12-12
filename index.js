783add7b5bfe85e3836661b096abed2b5e23bb78
chesnokov-mx <kkalibri@icloud.com>
2022-12-12 17:25:50 +0300

    edw

--- index.js
+++ index.js
@@ -80,8 +80,15 @@ async function makeMagic(){
 const app = express();
 app.use(express.json());
 
-// Routes
-// app.use("/home", home);
+app.use("/home", home);
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    return res.status(200).json({
+        title: "Express Testing",
+        message: "The app is working properly!",
+    });
+});
 
 // connection
 const port = process.env.PORT || 9001;
